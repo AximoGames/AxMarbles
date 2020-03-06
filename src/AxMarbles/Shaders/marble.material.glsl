2f041390bd58e9e529a84040fac585bbb9854851
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-03-07 00:13:53 +0100

    dynamic material

--- src/AxMarbles/Shaders/marble.material.glsl
+++ src/AxMarbles/Shaders/marble.material.glsl
@@ -1,7 +1,3 @@
-// --- LIB ---
-
-// --- CALL ---
-
 color = BlendColor(texture(material.diffuse, TexCoords).rgb, material.color, material.colorBlendMode);
 
 if((Normal.y>0 && Normal.x>0) || (Normal.y<0 && Normal.x<0))
