748d3d985786e868d75754c3568ffb8e5e81e6eb
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-03-30 00:58:32 +0200

    shader works

--- Assets/Shaders/marble.material.glsl
+++ Assets/Shaders/marble.material.glsl
@@ -1,6 +1,6 @@
-color = BlendColor(texture(material.diffuse, TexCoords).rgb, material.color, material.colorBlendMode);
+matDiffuse = BlendColor(texture(material.diffuse, TexCoords).rgb, material.color, material.colorBlendMode);
 
-if(joker==0.0)
+if(joker == 0.0)
 {
     if(
         (Normal.y>0 && Normal.x>0 && Normal.z>0)
@@ -8,9 +8,9 @@ if(joker==0.0)
         ||  (Normal.y>0 && Normal.x<0 && Normal.z<0)
         ||  (Normal.y<0 && Normal.x>0 && Normal.z<0)
         )
-        color = color2;}
+        matDiffuse = color2;}
 else
 {
-    color = abs(Normal.xyz) * 0.8;
+    matDiffuse = abs(Normal.xyz) * 0.8;
 }
 
